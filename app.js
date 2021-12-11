f0428fcbfb09e5ca619a4aacdd6e426f904315ab
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-12-11 23:59:48 +0200

    feat(back-end): Register with username

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+let usernames = [];
+
 io.on("connection", (socket) => {
-  // ...
+  console.log(`${socket.id} connected`);
+  
+  socket.on("enqueue", data => {
+    let { username } = data;
+    
+    if (usernames.indexOf(username) !== -1) {
+      socket.emit("error", "Username is already taken");
+      return;
+    }
+
+    usernames.push(username);
+    socket.emit("in_queue");
+  });
+
 });
 
 module.exports = { httpServer };
