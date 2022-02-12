8869354512b0492b5d91f4e6af42c940d9d120ac
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2022-02-13 01:41:22 +0200

    fix: check if round exists first on rounds before access getCorrectChoice() method

--- structures/Contest.structure.js
+++ structures/Contest.structure.js
@@ -1,85 +1,87 @@
 const { Round } = require("./Round.structure");
 
 class Contest {
-    
-    constructor (contestId) {
-        this._hasContestStarted = false;
-        this._readyPlayers = new Set();
-        this._score = new Map();
-        this._blockedPlayers = new Set();
-        this._rounds = [];
-        this._id = contestId;
-    }
+  constructor(contestId) {
+    this._hasContestStarted = false;
+    this._readyPlayers = new Set();
+    this._score = new Map();
+    this._blockedPlayers = new Set();
+    this._rounds = [];
+    this._id = contestId;
+  }
 
-    getId () {
-        return this._id;
-    }
+  getId() {
+    return this._id;
+  }
 
-    addPlayer (username) {
-        this._score.set(username, 0);
-    }
+  addPlayer(username) {
+    this._score.set(username, 0);
+  }
 
-    makeReady (username) {
-        this._readyPlayers.add(username);
-    }
+  makeReady(username) {
+    this._readyPlayers.add(username);
+  }
 
-    isReady (username) {
-        return this._readyPlayers.has(username);
-    }
+  isReady(username) {
+    return this._readyPlayers.has(username);
+  }
 
-    getReadyPlayersCount () {
-        return this._readyPlayers.size;
-    }
+  getReadyPlayersCount() {
+    return this._readyPlayers.size;
+  }
 
-    startContest () {
-        this._hasContestStarted = true;
-    }
+  startContest() {
+    this._hasContestStarted = true;
+  }
 
-    hasContestStarted () {
-        return this._hasContestStarted;
-    }
+  hasContestStarted() {
+    return this._hasContestStarted;
+  }
 
-    /**
-     * 
-     * @param round The details of the round to be added
-     */
-    addRound (round) {
-        this._rounds.push(round);
-        this._hasContestStarted = true;
-        this._blockedPlayers.clear();
-    }
+  /**
+   *
+   * @param round The details of the round to be added
+   */
+  addRound(round) {
+    this._rounds.push(round);
+    this._hasContestStarted = true;
+    this._blockedPlayers.clear();
+  }
 
-    getRoundsCount () {
-        return this._rounds.length;
-    }
+  getRoundsCount() {
+    return this._rounds.length;
+  }
 
-    addPointToPlayer (username) {
-        this._score.set(username, this._score.get(username) + 1);
-    }
+  addPointToPlayer(username) {
+    this._score.set(username, this._score.get(username) + 1);
+  }
 
-    isCorrectChoice (choice) {
-        return this._rounds[this._rounds.length - 1].getCorrectChoice() === choice;
-    }
+  isCorrectChoice(choice) {
+    return (
+      this._rounds[this._rounds.length - 1] &&
+      this._rounds[this._rounds.length - 1].getCorrectChoice() === choice
+    );
+  }
 
-    getScore () {
-        let obj = {};
-        for (let [key, value] of this._score) {
-            obj[key] = value;
-        }
-        return obj;
+  getScore() {
+    let obj = {};
+    for (let [key, value] of this._score) {
+      obj[key] = value;
     }
+    return obj;
+  }
 
-    blockPlayer (username) {
-        this._blockedPlayers.add(username);
-    }
+  blockPlayer(username) {
+    this._blockedPlayers.add(username);
+  }
 
-    isPlayerBlocked (username) {
-        return this._blockedPlayers.has(username);
-    }
+  isPlayerBlocked(username) {
+    return this._blockedPlayers.has(username);
+  }
 
-    getBlockedPlayersCount () {
-        return this._blockedPlayers.size;
-    }
+  getBlockedPlayersCount() {
+    return this._blockedPlayers.size;
+  }
 }
 
 module.exports = { Contest };
